ff1b18600773fd1c332d7d31062845f5c90ebbf7
NarendraUnnava <89415190+NarendraUnnava@users.noreply.github.com>
2021-11-03 21:21:20 -0500

    updated addmods  file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carRouter = require('./routes/car');
 var selectorRouter = require('./routes/selector');
+var addmodsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/car', carRouter);
 app.use('/selector', selectorRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
